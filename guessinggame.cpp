639d74ed2cab7b4b1b610e4bb645a3c112313ed0
Kavya <117969882+Kavya-812@users.noreply.github.com>
2022-12-25 22:35:24 +0530

    Add files via upload

---
+++ guessinggame.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace  std;
+
+int main(){
+    int secretNum=7;
+    int guess;
+    int guessCount =0;
+    int guessLimit =3;
+    bool outOfGuess= false;
+
+    while(secretNum != guess && !outOfGuess){
+        if (guessCount < guessLimit)
+        {
+            cout<<"Enter guess:";
+            cin>>guess;
+            guessCount++;
+
+        }
+        else
+        {
+            outOfGuess =true;
+
+        }
+    }
+
+    if (outOfGuess)
+    {
+        cout<<"You Lose";
+    }
+    else
+    {
+        cout<<"you win";
+    }
+    
+    
+    return 0;
+}
